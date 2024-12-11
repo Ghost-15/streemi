ee12a050aa29c6669c0c2753137fb1333acb3caf
TimosLeBoss <tatibatchi15@hotmail.com>
2024-12-11 09:19:53 +0100

    Bien joue negro

---
+++ src/Entity/Comment.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\Status;
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+    #[ORM\Column(type: 'string', enumType: Status::class)]
+    private Status $status;
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getStatus(): Status
+    {
+        return $this->status;
+    }
+
+    public function setStatus(Status $status): void
+    {
+        $this->status = $status;
+    }
+
+}
---
+++ src/Enum/AccountStatus.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enum;
+
+enum AccountStatus: string {
+    case DISPONIBLE = 'disponible';
+    case INDISPONIBLE = 'indisponible';
+}
