8144db1ffc3b44fca22850fa361da1b58ee4b03d
TimosLeBoss <tatibatchi15@hotmail.com>
2025-01-15 00:35:36 +0100

    Bien joue negro

--- src/Controller/HubController.php
+++ src/Controller/HubController.php
@@ -7,9 +7,12 @@ use App\Entity\Media;
 use App\Entity\Playlist;
 use App\Entity\PlaylistSubscription;
 use App\Entity\Subscription;
+use App\Entity\User;
 use App\Repository\MediaRepository;
 use App\Repository\PlaylistMediaRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,30 +29,12 @@ class HubController extends AbstractController
             'popularMedias' => $popularMedias,
         ]);
     }
-    #[Route('/subscription', name: 'app_subscription')]
-    public function subscription(EntityManagerInterface $entityManager): Response
-    {
-        $subscriptions = $entityManager->getRepository(Subscription::class)->findAll();
-        return $this->render('hub/subscriptions.html.twig', [
-            'subscriptions' => $subscriptions,
-        ]);
-    }
     #[Route('/discover', name: 'app_discover')]
     public function discover(EntityManagerInterface $entityManager): Response
     {
         $categories = $entityManager->getRepository(Categorie::class)->findAll();
         return $this->render('hub/discover.html.twig', [
             'categories' => $categories,
-        ]);
-    }
-    #[Route('/categorie/{value}', name: 'app_categorie')]
-    public function categorie(EntityManagerInterface $entityManager, MediaRepository $mediaRepository, string $value): Response
-    {
-        $categorie = $entityManager->getRepository(Categorie::class)->findOneBy(['name' => $value]);
-        $medias = $mediaRepository->findAllWhereMediaType(['value' => $value]);
-        return $this->render('hub/category.html.twig', [
-            'categorie' => $categorie,
-            'medias' => $medias,
         ]);
     }
     #[Route('/list', name: 'app_list')]
@@ -61,26 +46,68 @@ class HubController extends AbstractController
         $medias = [];
         if ($selectedPlaylistId) {
             $playlistMediaArray = $playlistMediaRepository->findAllMediaWhere(['value'=>$selectedPlaylistId]);
-        if ($playlistMediaArray) {
-            foreach ($playlistMediaArray as $playlistMedia) {
-                $m = $playlistMedia->getMedia();
-                $medias[] = [
-                    'id' => $m->getId(),
-                    'mediaType' => $m->getMediaType(),
-                    'title' => $m->getTitle(),
-                    'shortDescription' => $m->getShortDescription(),
-                    'longDescription' => $m->getLongDescription(),
-                    'releaseDate' => $m->getReleaseDate()->format('Y-m-d'),
-                    'coverImage' => $m->getCoverImage()
-                 ];
+            if ($playlistMediaArray) {
+                foreach ($playlistMediaArray as $playlistMedia) {
+                    $m = $playlistMedia->getMedia();
+                    $medias[] = [
+                        'id' => $m->getId(),
+                        'mediaType' => $m->getMediaType(),
+                        'title' => $m->getTitle(),
+                        'shortDescription' => $m->getShortDescription(),
+                        'longDescription' => $m->getLongDescription(),
+                        'releaseDate' => $m->getReleaseDate()->format('Y-m-d'),
+                        'coverImage' => $m->getCoverImage()
+                    ];
+                }
             }
-          }
         }
 
         return $this->render('hub/lists.html.twig', [
             'playlists' => $playlists,
             'playlistSubscriptions' => $playlistSubscriptions,
             'selectedPlaylistId' => $selectedPlaylistId,
+            'medias' => $medias,
+        ]);
+    }
+    #[Route('/profil', name: 'app_profil')]
+    public function profil(EntityManagerInterface $entityManager): Response
+    {
+        return $this->render('hub/profil.html.twig');
+    }
+    #[Route('/subscription', name: 'app_subscription')]
+    public function subscription(EntityManagerInterface $entityManager, Security $security): Response
+    {
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $security->getUser()->getUserIdentifier()]);
+        $subscriptions = $entityManager->getRepository(Subscription::class)->findAll();
+        return $this->render('hub/subscriptions.html.twig', [
+            'sub' => $user->getSubscription()->getName(),
+            'subscriptions' => $subscriptions,
+        ]);
+    }
+    #[Route('/subscription/{value}', name: 'app_subscription_user')]
+    public function subscriptionValue(EntityManagerInterface $entityManager, int $value, Security $security): Response
+    {
+        $subscription = $entityManager->getRepository(Subscription::class)->find($value);
+        $user = $security->getUser();
+        if($value){
+            $user->setSubscription($subscription);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', "C'est bon!");
+        } else {
+            $this->addFlash('error', 'Erreur...');
+        }
+        return $this->render('hub/subscriptions.html.twig');
+    }
+    #[Route('/categorie/{value}', name: 'app_categorie')]
+    public function categorie(EntityManagerInterface $entityManager, MediaRepository $mediaRepository, string $value): Response
+    {
+        $categorie = $entityManager->getRepository(Categorie::class)->findOneBy(['name' => $value]);
+        $medias = $mediaRepository->findAllWhereMediaType(['value' => $value]);
+        return $this->render('hub/category.html.twig', [
+            'categorie' => $categorie,
             'medias' => $medias,
         ]);
     }
